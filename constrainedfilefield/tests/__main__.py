e042b147bd4e2493dbf4a1602c7e502621a07087
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-03-01 16:25:24 +0100

    Update tests main

--- constrainedfilefield/tests/__main__.py
+++ constrainedfilefield/tests/__main__.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import os
 
-from django.core.management import call_command
-
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 if __name__ == "__main__":
-    call_command('test', 'tests')
+    import sys
+    from django import setup
+    from django.test.runner import DiscoverRunner
+
+    setup()
+    sys.exit(DiscoverRunner(verbosity=1).run_tests(['constrainedfilefield']))
