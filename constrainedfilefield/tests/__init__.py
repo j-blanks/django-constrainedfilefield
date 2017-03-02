f837eda9d0c22d4b3303b156e3f3dfddc1f2772c
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-03-02 09:50:00 +0100

    Should fix ImportError

--- constrainedfilefield/tests/__init__.py
+++ constrainedfilefield/tests/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from .models import TestModel, TestModelNoValidate, TestElement, TestContainer
+from .tests import ValidatedFileFieldTest
--- constrainedfilefield/tests/settings.py
+++ constrainedfilefield/tests/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'constrainedfilefield',
+    'tests',
 ]
 
 LANGUAGE_CODE = 'en'
