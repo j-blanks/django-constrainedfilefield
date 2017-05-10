41ba77d3e9d04d0f19a2a04610ba8f4f2246d21d
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-05-10 17:24:00 +0200

    Fix #2

--- constrainedfilefield/fields.py
+++ constrainedfilefield/fields.py
@@ -39,7 +39,7 @@ class ConstrainedFileField(models.FileField):
 
     """
 
-    description = "A FileField with constraints on size and/or type"
+    description = _("A FileField with constraints on size and/or type")
 
     def __init__(self, *args, **kwargs):
         self.max_upload_size = kwargs.pop("max_upload_size", 0)
@@ -113,3 +113,9 @@ class ConstrainedFileField(models.FileField):
         if self.js_checker:
             kwargs["js_checker"] = self.js_checker
         return name, path, args, kwargs
+
+    def __str__(self):
+        if hasattr(self, 'model'):
+            return super(ConstrainedFileField).__str__()
+        else:
+            return self.__class__.__name__
