ac61d67d64aab9b8ead0d994fd6003c4dc623129
Marc Bourqui <pypi.kemar@bourqui.org>
2018-01-02 18:08:32 +0100

    Bump to v3.1.3

--- constrainedfilefield/__init__.py
+++ constrainedfilefield/__init__.py
@@ -3,5 +3,5 @@
 
 __license__ = 'BSD-3-Clause'
 __author__ = 'Marc Bourqui'
-__version__ = '3.1.0'
+__version__ = '3.1.3'
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
