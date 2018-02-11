a3e3b38bcfe87974748da10e0daec193edb1145e
Marc Bourqui <pypi.kemar@bourqui.org>
2018-02-11 12:10:10 +0100

    Support install from repository with fallback readmes

--- setup.py
+++ setup.py
@@ -9,8 +9,14 @@ from constrainedfilefield import __version__
 
 REPO_URL = "https://github.com/mbourqui/django-constrainedfilefield/"
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
+README = ''
+for ext in ['md','rst']:
+	try:
+		with open(os.path.join(os.path.dirname(__file__), 'README.' + ext)) as readme:
+	    	README = readme.read()
+	except FileNotFoundError as fnfe:
+		pass
+
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
