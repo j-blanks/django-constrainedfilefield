a00fc55ee37e720d10b6d3d6353702d123a5b2ff
Marc Bourqui <pypi.kemar@bourqui.org>
2020-06-01 17:31:50 +0200

    Set doc theme to RTD

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ version = ".".join(release.split(".")[:2])
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",  # RTD look and feel
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -63,7 +64,7 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
