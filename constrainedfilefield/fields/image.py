173c4d789ffd277afd45e92dae9edbd9e52b3478
j-blanks <19312719+j-blanks@users.noreply.github.com>
2022-05-07 17:03:24 +0200

    fixed import gettext_lazy for Django 4.X

--- constrainedfilefield/fields/image.py
+++ constrainedfilefield/fields/image.py
@@ -7,7 +7,7 @@ from django import forms
 from django.conf import settings
 from django.db import models
 from django.template.defaultfilters import filesizeformat
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class ConstrainedImageField(models.ImageField):
